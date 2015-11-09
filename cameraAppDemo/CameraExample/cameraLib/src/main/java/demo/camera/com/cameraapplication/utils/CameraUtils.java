29651b294d8ba0f7e422ee952038725a9b914e2a
Rajnish <rajnish.iitg@gmail.com>
2015-11-09 12:00:59 +0530

    Added Utils

--- cameraAppDemo/CameraExample/cameraLib/src/main/java/demo/camera/com/cameraapplication/utils/CameraUtils.java
+++ cameraAppDemo/CameraExample/cameraLib/src/main/java/demo/camera/com/cameraapplication/utils/CameraUtils.java
@@ -17,11 +17,18 @@
 package demo.camera.com.cameraapplication.utils;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Build;
 import android.util.Log;
 
+import com.google.common.io.Files;
+
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import demo.camera.com.cameraapplication.encoder.SessionConfig;
@@ -137,5 +144,148 @@ public class CameraUtils {
 
         Log.d(TAG, "Couldn't find match for " + desiredThousandFps + ", using " + guess);
         return guess;
+    }
+
+    public static void moveVideoChunk(Context context, SessionConfig config) {
+        SessionConfig oldConfig = CameraUtils.getSessionConfig(context);
+        String path = oldConfig.getOutputPath();
+        File outputDirectory = new File(context.getExternalFilesDir(null), SessionConfig.sSessionFolder);
+        try {
+            outputDirectory.mkdirs();
+            File outPutFileName = new File(outputDirectory, System.currentTimeMillis() + ".mp4");
+            Files.move(new File(path), outPutFileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String stichVideos(Context context, String inputDirPath, String outputDirPath) {
+        final File inputDir = new File(inputDirPath);
+        final File outDir = new File(outputDirPath);
+
+        Log.d(TAG, "path=" + outDir.toString());
+        File outputFile = new File(outDir, "testOut" + System.currentTimeMillis() + ".mp4");
+
+        try {
+
+            // Clean Up
+            for (File inputFile : inputDir.listFiles()) {
+                if (!inputFile.exists()) {
+                    continue ;
+                }
+
+                if (!inputFile.getAbsolutePath().endsWith(".mp4")) {
+                    Log.d(TAG, "Deleting: " + inputFile.getPath());
+                    inputFile.delete();
+                    continue;
+                }
+
+                if (inputFile.length() == 0) {
+                    Log.d(TAG, "Deleting: " + inputFile.getPath());
+                    inputFile.delete();
+                    continue;
+                }
+            }
+
+
+            ArrayList<String> inputVideolist = new ArrayList<>();
+            for (File inputFile : inputDir.listFiles()) {
+                inputVideolist.add(inputFile.getPath());
+            }
+
+            if (inputVideolist.size() == 0) {
+                Log.e(TAG, "no video's found for stiching");
+                return null;
+            }
+
+            Mp4ParserUtility.stitchVideos(outputFile.getPath(), inputVideolist);
+
+            // Removes input files
+            for (File inputFile : inputDir.listFiles()) {
+                if (inputFile.exists()) {
+                    inputFile.delete();
+                }
+            }
+
+            Intent broadcastIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            broadcastIntent.setData(Uri.fromFile(outputFile));
+            context.sendBroadcast(broadcastIntent);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return outputFile.getPath();
+    }
+
+    public static float dp2px(Resources resources, float dp) {
+        final float scale = resources.getDisplayMetrics().density;
+        return  dp * scale + 0.5f;
+    }
+
+    public static float sp2px(Resources resources, float sp){
+        final float scale = resources.getDisplayMetrics().scaledDensity;
+        return sp * scale;
+    }
+
+    public static String millisecondToTimeString(long milliSeconds) {
+        long seconds = milliSeconds / 1000;
+        long minutes = seconds / 60;
+        long hours = minutes / 60;
+        long remainderMinutes = minutes - (hours * 60);
+        long remainderSeconds = seconds - (minutes * 60);
+
+        StringBuilder timeStringBuilder = new StringBuilder();
+
+        // Hours
+        if (hours > 0) {
+            if (hours < 10) {
+                timeStringBuilder.append('0');
+            }
+            timeStringBuilder.append(hours);
+
+            timeStringBuilder.append(':');
+        }
+
+        // Minutes
+        if (remainderMinutes < 10) {
+            timeStringBuilder.append('0');
+        }
+        timeStringBuilder.append(remainderMinutes);
+        timeStringBuilder.append(':');
+
+        // Seconds
+        if (remainderSeconds < 10) {
+            timeStringBuilder.append('0');
+        }
+        timeStringBuilder.append(remainderSeconds);
+
+        return timeStringBuilder.toString();
+    }
+
+    public static void clearSessionFolders(Context context) {
+        final File sessionFolder = new File(context.getExternalFilesDir(null),
+                SessionConfig.sSessionFolder);
+
+        if (sessionFolder != null && sessionFolder.exists()) {
+            for (File file : sessionFolder.listFiles()) {
+                if (file.exists()) {
+                    file.delete();
+                }
+            }
+        }
+
+
+        final File sessionFolderTemp = new File(context.getExternalFilesDir(null),
+                SessionConfig.sSessionFolderTemp);
+        if (sessionFolderTemp != null && sessionFolderTemp.exists()) {
+            for (File file : sessionFolderTemp.listFiles()) {
+                if (file.exists()) {
+                    file.delete();
+                }
+            }
+        }
+
     }
 }
